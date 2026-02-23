d4854daf8e7a639b05069f9c036ef7d3fc0830dd
Prashant1817 <prashanttorar420@gmail.com>
2026-02-23 19:15:43 +0530

    Create script.js

---
+++ JavaScript/script.js
@@ -0,0 +1,42 @@
+// function message() {
+//     alert("Submitted successfully ");
+
+
+
+console.log("Hello")
+console.log(3+7)
+
+
+document.writeln("Hello All, How are You") //  statement
+document.writeln("<br>")//tag
+document.writeln("<br> Thank You")
+document.writeln("<br>")
+document.writeln("<h3> Hello All<h3>")// element
+document.writeln("<br>")
+document.writeln(3+7)
+document.writeln("<br>")
+document.writeln("3+7")
+document.writeln("<br>")
+document.writeln(3 + "3" + 4 + 7)
+document.writeln("<br>")
+document.writeln("3" + 1 + 2 + 3)
+document.writeln("<br>")
+document.writeln(1 + 4 + 5 + "3")
+
+
+// function hello() {
+//     var a = 10;
+// }
+
+// hello() 
+// console.log(a);
+
+// }
+
+
+// function hello() {
+//     var a = 10;
+// }
+
+// hello() 
+// console.log(a);
